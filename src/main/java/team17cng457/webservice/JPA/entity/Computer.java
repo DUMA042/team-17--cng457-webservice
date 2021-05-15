ce8419d8a58fc433d3bffcaf35c30dae4e0cd6d3
Ata Hakçıl <ahakcil@gmail.com>
2021-05-15 17:09:48 +0300

    Remove inherited member variables from entity.Computer

--- src/main/java/team17cng457/webservice/JPA/entity/Computer.java
+++ src/main/java/team17cng457/webservice/JPA/entity/Computer.java
@@ -8,26 +8,10 @@ import javax.persistence.Table;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 
-@Table(name = "computer")
+@Table(name = "device")
 
 @Entity
 public class Computer extends Device{
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    @Column(name = "computer_id")
-    private int computer_id;
-
-    @Column(name = "brand")
-    private String brand;
-
-    @Column(name = "model")
-    private String model;
-
-    @Column(name = "screen_size")
-    private String screenSize;
-
-    @Column(name = "price")
-    private float price;
 
     @Column(name = "storage_capacity")
     private int storageCapacity;
