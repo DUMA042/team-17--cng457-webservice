8942fbcd3ab18757b7f991e133b44015119f96fb
Ata Hakçıl <ahakcil@gmail.com>
2021-05-15 16:59:33 +0300

    Implement entity.Device primitive types

--- src/main/java/team17cng457/webservice/JPA/entity/Device.java
+++ src/main/java/team17cng457/webservice/JPA/entity/Device.java
@@ -1,4 +1,24 @@
 package team17cng457.webservice.JPA.entity;
 
+import javax.persistence.*;
+
+@MappedSuperclass
 public class Device {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "device_id")
+    private int computer_id;
+
+    @Column(name = "brand")
+    private String brand;
+
+    @Column(name = "model")
+    private String model;
+
+    @Column(name = "screen_size")
+    private String screenSize;
+
+    @Column(name = "price")
+    private float price;
+
 }
