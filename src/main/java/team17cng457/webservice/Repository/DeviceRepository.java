da8b7a77a2ea429a00850126756751ec5f2e1ade
DUMA042 <dumebiikeoffiah@gmail.com>
2021-05-16 18:20:59 +0300

    Merge remote-tracking branch 'origin/Phone_and_Additionfeature' into Phone_and_Additionfeature(remove findAll sort)

--- src/main/java/team17cng457/webservice/Repository/DeviceRepository.java
+++ src/main/java/team17cng457/webservice/Repository/DeviceRepository.java
@@ -3,7 +3,7 @@ package team17cng457.webservice.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import team17cng457.webservice.JPA.entity.Device;
 
-import java.util.List;
+
 
 public interface DeviceRepository extends JpaRepository<Device,Long> {
 
