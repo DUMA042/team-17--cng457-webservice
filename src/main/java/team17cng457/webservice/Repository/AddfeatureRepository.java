0e5e5f6751ba5035f2a4e95c82e984140baca19b
DUMA042 <dumebiikeoffiah@gmail.com>
2021-05-16 16:54:16 +0300

    This is the finished form of Phase II.Has all the Entity,Service and Repository,Also has come query.(ANTHONY Branch

---
+++ src/main/java/team17cng457/webservice/Repository/AddfeatureRepository.java
@@ -0,0 +1,8 @@
+package team17cng457.webservice.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import team17cng457.webservice.JPA.entity.AdditionalFeature;
+
+public interface AddfeatureRepository extends JpaRepository<AdditionalFeature,Double> {
+
+}
