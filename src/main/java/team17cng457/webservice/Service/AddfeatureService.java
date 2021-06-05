79d81c48a02f8d12e4fe56e57290c9b06ad87777
Ata Hakçıl <ahakcil@gmail.com>
2021-06-05 21:22:25 +0300

    Add missing service keywords

--- src/main/java/team17cng457/webservice/Service/AddfeatureService.java
+++ src/main/java/team17cng457/webservice/Service/AddfeatureService.java
@@ -1,9 +1,11 @@
 package team17cng457.webservice.Service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import team17cng457.webservice.JPA.entity.AdditionalFeature;
 import team17cng457.webservice.Repository.AddfeatureRepository;
 
+@Service
 public class AddfeatureService {
     @Autowired
     private AddfeatureRepository featurerep;
--- src/main/java/team17cng457/webservice/Service/Comment_Service.java
+++ src/main/java/team17cng457/webservice/Service/Comment_Service.java
@@ -1,11 +1,13 @@
 package team17cng457.webservice.Service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import team17cng457.webservice.JPA.entity.Comment;
 import team17cng457.webservice.JPA.entity.Device;
 import team17cng457.webservice.Repository.CommentRepository;
 import team17cng457.webservice.Repository.DeviceRepository;
 
+@Service
 public class Comment_Service {
     @Autowired
     private CommentRepository Commentrep;
