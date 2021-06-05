f39eee0918e201d50b082cc85db164b64cac2a06
Ata Hakçıl <ahakcil@gmail.com>
2021-06-05 15:36:01 +0300

    Add search related JPA functions

--- src/main/java/team17cng457/webservice/Repository/DeviceRepository.java
+++ src/main/java/team17cng457/webservice/Repository/DeviceRepository.java
@@ -3,9 +3,18 @@ package team17cng457.webservice.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import team17cng457.webservice.JPA.entity.Device;
 
+import java.util.List;
 
 
 public interface DeviceRepository extends JpaRepository<Device,Long> {
 
+    //find all computers or phones
+    public List<Device> findBydevice_type(int device_type);
+
+    //find specific device (phone or computer)
+    public List<Device> findBydevice_id(int device_id);
+
+    //find phone or computer by brand
+    public List<Device> findBydevice_typeAndbrand(int device_type, String brand);
 
 }
