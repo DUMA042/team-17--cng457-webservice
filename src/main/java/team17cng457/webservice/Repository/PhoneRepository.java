2c8c309152296d7f9c41e0d190502204c7587873
Ata Hakçıl <ahakcil@gmail.com>
2021-06-05 18:03:03 +0300

    Remove unneccessary controller and repositories

--- src/main/java/team17cng457/webservice/Repository/PhoneRepository.java
+++
@@ -1,8 +0,0 @@
-package team17cng457.webservice.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import team17cng457.webservice.JPA.entity.Phone;
-
-public interface PhoneRepository extends JpaRepository<Phone,Long> {
-
-}
